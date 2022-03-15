f561ab47ca3333ccafa4249b4940edc3c743547e
IcemanMick <doylewithad@gmail.com>
2022-03-15 00:01:27 +0000

    Run further testing for debugging and final commit of the day

--- run.py
+++ run.py
@@ -45,8 +45,6 @@ def get_batman_rating():
     print("Please answer each question with a rating between 1 to 10.")
     print("1 being the lowest score and 10 being the highest score.\n")
 
-    print("The first section is on acting performances...\n")
-
     while True:
         batman = input("Please rate Robert Pattinson as Batman?: ")
 
@@ -82,3 +80,4 @@ def validate_rating(values):
 
 
 data = get_batman_rating()
+print(data)
