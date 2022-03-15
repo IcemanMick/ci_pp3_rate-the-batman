3c94137acbc4ab83397c60e7e62b1b46ce869e65
IcemanMick <doylewithad@gmail.com>
2022-03-15 20:17:17 +0000

    Add validation for exactly 3 ratings to be entered in survey

--- run.py
+++ run.py
@@ -22,10 +22,25 @@ def get_sales_data():
     print("1 being the lowest score and 10 being the highest score.\n")
     print("Ratings should be 3 numbers for A, B, & C, separated by commas")
     print("Example: 10,9,7\n")
-    print("Please rate A) Batman B) Catwoman C) The Riddler\n")
+    print("Please rate: A) Batman B) Catwoman C) The Riddler\n")
 
     data_str = input("Enter your three ratings here: ")
-    print(f"The ratings provided are {data_str}")
+
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    """
+    Inside try converts all strings to integers.
+    """
+    try:
+        if len(values) != 3:
+            raise ValueError(
+                f"3 ratings should be entered, you gave {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid number of ratings: {e}. Please try again.\n")
 
 
 get_sales_data()
