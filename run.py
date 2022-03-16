a8aa9e2246595e6479218c83ecb560deaa3ced15
IcemanMick <doylewithad@gmail.com>
2022-03-16 11:49:36 +0000

    Add get overall score function and begin testing it

--- run.py
+++ run.py
@@ -23,13 +23,14 @@ def get_sales_data():
     print("1 being the lowest score and 10 being the highest score.\n")
     print("Ratings should be 3 numbers for A,B,C, separated by commas.")
     print("Example: 10,9,7\n")
+    print("Please rate the main characters here:")
     while True:
-        data_str = input("Rate: A)Batman,B)Catwoman,C)The Riddler, here: ")
+        data_str = input("A)Batman,B)Catwoman,C)The Riddler: ")
 
         sales_data = data_str.split(",")
 
         if validate_data(sales_data):
-            print("Ratings are valid!")
+            print("Ratings successfully recorded!")
             break
 
     return sales_data
@@ -63,20 +64,21 @@ def update_worksheet(data, worksheet):
     print(f"Updating {worksheet} worksheet...\n")
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
-    print(f"{worksheet} worksheet updated successfully")
+    print(f"{worksheet} worksheet updated successfully\n")
 
 
 def get_supporting_data():
     """
     rating supporting characters
     """
+    print("Please rate the supporting characters here:")
     while True:
-        data_str = input("Rate: A)Alfred,B)Penguin,C)Jim Gordon, here: ")
+        data_str = input("A)Alfred,B)Penguin,C)Jim Gordon: ")
 
         supporting_data = data_str.split(",")
 
         if validate_data(supporting_data):
-            print("Ratings are valid!")
+            print("Ratings successfully recorded!")
             break
 
     return supporting_data
@@ -86,13 +88,14 @@ def get_production_data():
     """
     rating production value of movie
     """
+    print("Please rate the production values here:")
     while True:
-        data_str = input("Rate: A)Costumes,B)Visuals,C)Music, here: ")
+        data_str = input("A)Costumes,B)Visuals,C)Music: ")
 
         production_data = data_str.split(",")
 
         if validate_data(production_data):
-            print("Ratings are valid!")
+            print("Ratings successfully recorded!")
             break
 
     return production_data
@@ -100,7 +103,7 @@ def get_production_data():
 
 def main():
     """
-    Run all functions for the survey
+    Run all functions
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
@@ -113,5 +116,16 @@ def main():
     update_worksheet(production_data, "production")
 
 
+def get_overall_score():
+    """
+    get overall score
+    """
+    print("Just one final rating needed,this time only 1 number is required")
+    print("Example: 8\n")
+    data_str = input("How would you overall rate The Batman movie? ")
+    overall_data = int(data_str)
+
+
 print("Welcome to Rate The Batman!\n")
-main()
+# main()
+get_overall_score()
