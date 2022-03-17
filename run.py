c9874a7da165a60f5381b396e74381f591191314
IcemanMick <doylewithad@gmail.com>
2022-03-17 22:17:46 +0000

    Commence code comments on get average rating function

--- run.py
+++ run.py
@@ -173,6 +173,8 @@ def calculate_total_rating(main_row):
     Institute. Code content is mix of custom and Code Institute.
     Lines 193 to 196 are custom code.
     """
+    # First part
+
     supporting = SHEET.worksheet("supporting").get_all_values()
     # Using gspread to access all data on "supporting" Google Sheet
     supporting_row = supporting[-1]
@@ -186,6 +188,8 @@ def calculate_total_rating(main_row):
         # Converts list of strings to integers and adds list of integers
         subtotal_data.append(subtotal)
         # Adds summation of subtotal variable to empty subtotal_data list
+
+    # Second part
 
     print("Calculating your total rating for The Batman...\n")
     production = SHEET.worksheet("production").get_all_values()
@@ -202,6 +206,8 @@ def calculate_total_rating(main_row):
         total_data.append(total)
         # Adds summation of total variable to empty total_data list
 
+    # Final part
+
     score = total_data
     total_score = sum(score)
     # Creates new variable and adds all integers in the list together
@@ -211,62 +217,89 @@ def calculate_total_rating(main_row):
 
 def get_average_rating():
     """
-    gets sum of each column on each worksheet for average score
-    calculation
+    Gets sum of each of the three columns on each worksheet for average score
+    calculation.
     """
+    # "main" worksheet
+
     main_sheet = SHEET.worksheet("main")
+    # Using gspread to access "main" Google Sheet
     batman_column = main_sheet.col_values(1)
+    # Using gspread to access column 1 on "main" Google Sheet
     batman_column.remove("batman")
+    # Remove the column heading string "batman" so column can be sum totalled
     for i in range(0, len(batman_column)):
         batman_column[i] = int(batman_column[i])
     a = sum(batman_column)
 
     catwoman_column = main_sheet.col_values(2)
+    # Using gspread to access column 2 on "main" Google Sheet
     catwoman_column.remove("catwoman")
+    # Remove the column heading string "catwoman" so column can be sum totalled
     for i in range(0, len(catwoman_column)):
         catwoman_column[i] = int(catwoman_column[i])
     b = sum(catwoman_column)
 
     riddler_column = main_sheet.col_values(3)
+    # Using gspread to access column 3 on "main" Google Sheet
     riddler_column.remove("the riddler")
+    # Remove the column heading string "riddler" so column can be sum totalled
     for i in range(0, len(riddler_column)):
         riddler_column[i] = int(riddler_column[i])
     c = sum(riddler_column)
 
+    # "supporting" worksheet
+
     support_sheet = SHEET.worksheet("supporting")
+    # Using gspread to access "supporting" Google Sheet
     alfred_column = support_sheet.col_values(1)
+    # Using gspread to access column 1 on "supporting" Google Sheet
     alfred_column.remove("alfred")
+    # Remove the column heading string "alfred" so column can be sum totalled
     for i in range(0, len(alfred_column)):
         alfred_column[i] = int(alfred_column[i])
     d = sum(alfred_column)
 
     penguin_column = support_sheet.col_values(2)
+    # Using gspread to access column 2 on "supporting" Google Sheet
     penguin_column.remove("penguin")
+    # Remove the column heading string "penguin" so column can be sum totalled
     for i in range(0, len(penguin_column)):
         penguin_column[i] = int(penguin_column[i])
     e = sum(penguin_column)
 
     gordon_column = support_sheet.col_values(3)
-    gordon_column.remove("jim gordon")
+    # Using gspread to access column 3 on "supporting" Google Sheet
+    gordon_column.remove("gordon")
+    # Remove the column heading string "gordon" so column can be sum totalled
     for i in range(0, len(gordon_column)):
         gordon_column[i] = int(gordon_column[i])
     f = sum(gordon_column)
 
+    # "production" worksheet
+
     production_sheet = SHEET.worksheet("production")
+    # Using gspread to access "production" Google Sheet
     visuals_column = production_sheet.col_values(1)
+    # Using gspread to access column 1 on "production" Google Sheet
     visuals_column.remove("visuals")
+    # Remove the column heading string "visuals" so column can be sum totalled
     for i in range(0, len(visuals_column)):
         visuals_column[i] = int(visuals_column[i])
     g = sum(visuals_column)
 
     costumes_column = production_sheet.col_values(2)
+    # Using gspread to access column 2 on "production" Google Sheet
     costumes_column.remove("costumes")
+    # Remove the column heading string "costumes" so column can be sum totalled
     for i in range(0, len(costumes_column)):
         costumes_column[i] = int(costumes_column[i])
     h = sum(costumes_column)
 
     music_column = production_sheet.col_values(3)
+    # Using gspread to access column 3 on "production" Google Sheet
     music_column.remove("music")
+    # Remove the column heading string "music" so column can be sum totalled
     for i in range(0, len(music_column)):
         music_column[i] = int(music_column[i])
     # print(sum(music_column))
